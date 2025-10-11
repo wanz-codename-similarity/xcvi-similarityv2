c76aca2010ccc87d662df972326612ccf49382e4
wanz-codename-similarity <xcvispport@gmail.com>
2025-10-11 21:33:39 +0700

    update port to 51147 via Telegram bot

--- api/connect.js
+++ api/connect.js
@@ -21,7 +21,7 @@ export const INFO_TEXT = "Yang menang itu yang di private, bukan di public #Wanz
 
 const config = {
   domain: "http://private2okto.kantinvps.my.id",
-  port: 3089,
+  port: 51147,
   creator: "Wanz Official",
 };
 const base = `${config.domain}:${config.port}`;
