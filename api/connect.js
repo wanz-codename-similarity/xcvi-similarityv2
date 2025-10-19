69d74bffb9f435eb063994042335b7ff59135bcd
wanz-codename-similarity <xcvispport@gmail.com>
2025-10-19 15:30:07 +0700

    Disable maintenance via Telegram bot

--- api/connect.js
+++ api/connect.js
@@ -15,7 +15,7 @@ const TELEGRAM_TOKEN = "8385900567:AAGMaqapR7VoEzJomae8TA40hvrgd31fxGQ";
 const OWNER_CHAT_ID = "7950114253";
 
 /* ====== MAINTENANCE CONFIG ====== */
-const IS_MAINTENANCE = true;
+const IS_MAINTENANCE = false;
 const BYPASS_ROLES = ["developer"];
 export const INFO_TEXT = "server tinggal finishing saja, enjoy!";
 
